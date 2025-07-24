7ede95173df5580e2295649ab5c823fa164f3d8f
Jace Rodgers <jaransas2008@gmail.com>
2025-07-23 20:32:53 -0500

    wired repo fix

---
+++ kt-blinky/src/main/java/frc/robot/RobotContainer.kt
@@ -0,0 +1,55 @@
+package frc.robot
+
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import frc.robot.subsystems.intake.Intake
+import frc.robot.subsystems.intake.IntakeIO
+import frc.robot.subsystems.intake.IntakeReal
+import frc.robot.subsystems.intake.IntakeReplay
+import frc.robot.subsystems.intake.IntakeSim
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the [Robot]
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ *
+ * In Kotlin, it is recommended that all your Subsystems are Kotlin objects. As such, there
+ * can only ever be a single instance. This eliminates the need to create reference variables
+ * to the various subsystems in this container to pass into to commands. The commands can just
+ * directly reference the (single instance of the) object.
+ */
+object RobotContainer {
+    val robotRunType = Robot.robotRunType
+    private val driverController = CommandXboxController(0);
+
+    private lateinit var intake: Intake;
+
+    init
+    {
+        when(robotRunType) {
+            Robot.RobotRunType.kReal -> {
+                intake = Intake(IntakeReal())
+            }
+            Robot.RobotRunType.kSimulation -> {
+                intake = Intake(IntakeSim())
+            }
+            Robot.RobotRunType.kReplay -> {
+                intake = Intake(IntakeReplay())
+            }
+        }
+
+        configureBindings()
+    }
+
+    /** Use this method to define your `trigger->command` mappings. */
+    private fun configureBindings()  {
+        driverController.b().onTrue(intake.intakeCMD(5.0))
+        driverController.a().onTrue(intake.intakeCMD(-5.0))
+    }
+
+    fun getAutonomousCommand(): Command? {
+        // TODO: Implement properly
+        return null
+    }
+}
